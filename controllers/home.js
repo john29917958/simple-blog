a1d2e718ac50b70926508f4f12f21b81a5e485d6
Tzu-Chao Wang <john29917958@gmail.com>
2024-10-17 17:19:33 +0800

    Limit posts bodies content sizes
    Limit each post body content size to 3 HTML elements. This change is
    applied to home page.

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,5 @@
 const BlogPost = require("../models/BlogPost");
+const htmlParser = require("node-html-parser");
 
 module.exports = async (req, res) => {
   const blogPosts = await BlogPost.find({
@@ -14,7 +15,34 @@ module.exports = async (req, res) => {
     .sort({ datePosted: -1 })
     .populate("userid")
     .exec();
+
+  addPreviewBodies(blogPosts);
+
   res.render("index", {
     blogPosts,
   });
 };
+
+function addPreviewBodies(blogPosts) {
+  for (let blogPost of blogPosts) {
+    addPreviewBody(blogPost);
+  }
+}
+
+function addPreviewBody(blogPost) {
+  const postBody = htmlParser.parse(blogPost.body);
+  const previewPostBodyElems = getPreviewPostBodyElems(postBody);
+  const previewPostBody = htmlParser.parse("");
+  for (let previewPostBodyElem of previewPostBodyElems) {
+    previewPostBody.appendChild(previewPostBodyElem);
+  }
+  blogPost.previewBody = previewPostBody.toString();
+}
+
+function getPreviewPostBodyElems(postBody) {
+  const firstThreeElems = [];
+  for (let i = 0; i < 3 && i < postBody.childNodes.length; i += 1) {
+    firstThreeElems.push(postBody.childNodes[i]);
+  }
+  return firstThreeElems;
+}
